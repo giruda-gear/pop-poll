ada9eac2e2def6e93e3fe4f4ccca43035d5a5a4a
Hyeongsu Kim <53201140+giruda-gear@users.noreply.github.com>
2025-06-27 16:12:43 +0200

    test: separate unit and integration Jest configs

---
+++ apps/api/poll/jest.e2e.config.ts
@@ -0,0 +1,18 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+const { compilerOptions } = require('../../../tsconfig.base.json');
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '../../../', 
+  testRegex: '.*\\e2e.spec\\.ts$',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/apps/api/poll/tsconfig.spec.json',
+      },
+    ],
+  },
+};
